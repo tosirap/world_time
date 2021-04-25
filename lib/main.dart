d716d9d88bd72698347690089fa22ca09b38cca4
tosirap <parisotpaul@live.fr>
2021-04-25 16:51:05 +0200

    project finish

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/loading.dart';
+import 'pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+
+    },
+  ));
+}
